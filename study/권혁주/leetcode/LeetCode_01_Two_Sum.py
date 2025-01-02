35dbc58bed7960cc1723805b317843bcfe9f7252
kwonjoo <danhjkwon@naver.com>
2025-01-02 18:31:55 +0900

    리트코드 1번 Two_Sum

--- study/권혁주/leetcode/LeetCode_01_Two_Sum.py
+++ study/권혁주/leetcode/LeetCode_01_Two_Sum.py
@@ -3,4 +3,4 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j]  #주석
+                    return [i, j]  #안녕하세요
